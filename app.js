2e4356096cb5bf93383e10bdb98adc0de6d6d0e9
DanielLeonP <60786270+DanielLeonP@users.noreply.github.com>
2022-03-26 06:06:34 -0600

    Add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,12 @@ db = database.conectar();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'ProyectoHackathon',
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -27,14 +27,13 @@ router.get('/registro', function (req, res, next) {
 });
 
 router.post('/registro', function (req, res, next) {
-  let idUsuario = req.body.idUser
   let nombre = req.body.Nombre
   let apellido = req.body.Apellido
   let correo = req.body.email;
   let contrasena = req.body.password;
   let confirmarContrasena = req.body.confpassword
   //falta validar contraseña y mas datos para que no esten vacios
-  sql = "INSERT INTO Paciente VALUES (" + idUsuario + ", '" + nombre + "', '" + apellido + "', '" + correo + "', '" + contrasena + "');";
+  sql = "INSERT INTO Pacientes VALUES ('" + nombre + "', '" + apellido + "', '" + correo + "', '" + contrasena + "');";
   console.log(sql)
   db.query(sql,
     function (err, results, fields) {
@@ -47,12 +46,38 @@ router.post('/registro', function (req, res, next) {
       }
     })
 });
+router.get('/registroMedico', function (req, res, next) {
+  res.status(200);
+  res.render('registroMedico.ejs', { validacion: 'C' });
+});
+router.post('/registroMedico', function (req, res, next) {
+  let nombre = req.body.Nombre
+  let apellido = req.body.Apellido
+  let correo = req.body.email;
+  let contrasena = req.body.password;
+  let confirmarContrasena = req.body.confpassword;
+  let cedula = req.body.cedula;
+  let especialidad = req.body.especialidad;
+  //falta validar contraseña y mas datos para que no esten vacios
+  sql = "INSERT INTO Medicos (nombre, apellido, correo, contrasena, cedulaProfesional, especialidad) VALUES ('" + nombre + "', '" + apellido + "', '" + correo + "', '" + contrasena + "', " + cedula + ", '" + especialidad + "');";
+  console.log(sql)
+  db.query(sql,
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+        res.render('registroMedico.ejs', { validacion: 'I' });
+      } else {
+        res.status(200);
+        res.redirect('/medico');
+      }
+    })
+});
 
 router.post('/login', function (req, res, next) {
   let correo = req.body.email;
   let contrasena = req.body.password;
   console.log(correo + " y " + contrasena);
-  sql = "SELECT * FROM Paciente WHERE CorreoPaciente = '" + correo + "' AND " + " ContrasenaPaciente = '" + contrasena + "';";
+  sql = "SELECT * FROM Pacientes WHERE correo = '" + correo + "' AND " + " contrasena = '" + contrasena + "';";
   console.log(sql)
   db.query(sql,
     function (err, results, fields) {
@@ -60,9 +85,27 @@ router.post('/login', function (req, res, next) {
         throw err;
       } else {
         if (results.length == 0) {
-          res.status(400);
-          res.render('login.ejs', { validacion: 'I' });
+          sql = "SELECT * FROM Medicos WHERE correo = '" + correo + "' AND " + " contrasena = '" + contrasena + "';";
+          console.log(sql)
+          db.query(sql,
+            function (err, results, fields) {
+              if (err) {
+                throw err;
+              } else {
+                if (results.length == 0) {
+                  res.status(400);
+                  res.render('login.ejs', { validacion: 'I' });
+                } else {
+                  req.session.idUser = results[0].idMedico;                 
+                  req.session.tipoUser = "Medico";
+                  res.status(200);
+                  res.redirect('/medico');
+                }
+              }
+            })
         } else {
+          req.session.idUser = results[0].idPaciente; 
+          req.session.tipoUser = "Paciente";
           res.status(200);
           res.redirect('/menu');
         }
@@ -72,34 +115,53 @@ router.post('/login', function (req, res, next) {
 
 
 router.get('/menu', function (req, res, next) {
-  
+  console.log(req.session.idUser + " y " + req.session.tipoUser);
   res.render('Menu.ejs');
 });
 
-router.get('/alimetacion', function (req, res, next) {
-  res.render('alimetacion.ejs');
+router.get('/menu/alimentacion', function (req, res, next) {
+  id = req.session.idUser
+  res.render('alimentacion.ejs');
+  sql = `SELECT * FROM Alimentos WHERE IdAlimento IN (SELECT IdAlimento FROM AlimentosRecomendaciones WHERE IdRecomendacion IN (SELECT IdRecomendacion FROM SintomasRecomendaciones WHERE IdSintoma IN (SELECT IdSintoma FROM PacientesSintomas WHERE IdPaciente = ${id})))`;  
+  console.log(sql);
+  db.query(sql,
+    function (err, results, fields) {
+      if (err) {
+        throw err;
+      } else {
+        res.render('alimentacion.ejs', { alimentos: results });
+      }
+    })
 });
 
-router.get('/actividadFisica', function (req, res, next) {  
+router.get('/menu/actividadFisica', function (req, res, next) {
   res.render('actividadFisica.ejs');
 });
-router.get('/monitoreo', function (req, res, next) {
+router.get('/menu/monitoreo', function (req, res, next) {
   res.render('monitoreo.ejs');
 });
-router.get('/estres', function (req, res, next) {
-  res.render('estres.ejs');
+
+router.get('/menu/recomendaciones', function (req, res, next) {
+  res.render('recomendaciones.ejs');
 });
-router.get('/consejos', function (req, res, next) {
-  res.render('consejos.ejs');
+router.get('/menu/perfil', function (req, res, next) {
+  res.render('perfil.ejs');
 });
-router.get('/consulta', function (req, res, next) {
-  res.render('consulta.ejs');
+router.get('/menu/masInformacion', function (req, res, next) {
+  res.render('masInformacion.ejs');
 });
 router.get('/medico', function (req, res, next) {
+  console.log(req.session.idUser + " y " + req.session.tipoUser);
   res.render('medico.ejs');
 });
-router.get('/informacionGeneral', function (req, res, next) {
-  res.render('informacionGeneral.ejs');
+router.get('/medico/consultas', function (req, res, next) {
+  res.render('consultas.ejs');
+});
+router.get('/menu', function (req, res, next) {
+  res.render('Menu.ejs');
+});
+router.get('/listaMedicos', function (req, res, next) {
+  res.render('listaMedicos.ejs');
 });
 
 module.exports = router;
